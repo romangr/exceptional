eba5ea7789911b83a77abe982beabe7de89f5b29
romangr <romangr@users.noreply.github.com>
2019-12-14 02:05:25 +0300

    Some improvements and javadoc. (#3)

--- src/main/java/ru/romangr/exceptional/Exceptional.java
+++ src/main/java/ru/romangr/exceptional/Exceptional.java
@@ -1,13 +1,13 @@
 package ru.romangr.exceptional;
 
-import java.util.Objects;
-import javax.annotation.Nullable;
 import ru.romangr.exceptional.nullability.NonNullApi;
 import ru.romangr.exceptional.type.ExceptionalFunction;
 import ru.romangr.exceptional.type.ExceptionalSupplier;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import javax.annotation.Nullable;
 
 @NonNullApi
 public final class Exceptional<T> {
@@ -18,8 +18,14 @@ public final class Exceptional<T> {
   @Nullable
   private final T value;
 
+  private boolean isExceptionHandled;
+
   /**
-   * @param supplier should not be null
+   * Get some value from supplier catching all the exceptions.
+   *
+   * @param supplier to get value from.
+   * @param <V> type of the value.
+   * @return an instance of {@link Exceptional} with value or exception or in empty state.
    */
   public static <V> Exceptional<V> getExceptional(ExceptionalSupplier<V> supplier) {
     try {
@@ -29,31 +35,36 @@ public final class Exceptional<T> {
     }
   }
 
-  public static <V> Exceptional<V> getExceptional(ExceptionalSupplier<V> supplier,
-      int numberOfRetries) {
-    Exceptional<V> exceptional = Exceptional.getExceptional(supplier);
-    for (int i = 0; exceptional.isException() && i < numberOfRetries; i++) {
-      exceptional = exceptional.resumeOnException(e -> supplier.get());
-    }
-    return exceptional;
-  }
-
   /**
-   * @param value nullable
+   * Wrap null or some value with {@link Exceptional}.
+   *
+   * @param value to wrap.
+   * @param <V> type of the value.
+   * @return an instance of {@link Exceptional} with value or in empty state.
    */
   public static <V> Exceptional<V> exceptional(@Nullable V value) {
     return new Exceptional<>(value);
   }
 
   /**
-   * @param exception should not be null
+   * Wrap an exception with {@link Exceptional}.
+   *
+   * @param exception to wrap.
+   * @param <V> type of the exception.
+   * @return an instance of {@link Exceptional} with exception.
    */
   public static <V> Exceptional<V> exceptional(Exception exception) {
     return new Exceptional<>(exception);
   }
 
   /**
-   * @param mapper should not be null
+   * Map the value in {@link Exceptional} to some other value. Exceptions in mapper won't be
+   * caught.
+   *
+   * @param mapper to get a new value.
+   * @param <V> type of new value.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before mapping.
    */
   @SuppressWarnings("unchecked")
   public <V> Exceptional<V> map(Function<? super T, V> mapper) {
@@ -64,24 +75,30 @@ public final class Exceptional<T> {
   }
 
   /**
-   * Maps the current value catching all the exceptions from mapper
+   * Map the value in {@link Exceptional} to some other value catching all the exceptions from
+   * mapper.
    *
-   * @param mapper should be not null
+   * @param mapper to get a new value.
+   * @param <V> type of new value.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before mapping or with an exception occurred in process of mapping.
    */
   @SuppressWarnings("unchecked")
   public <V> Exceptional<V> safelyMap(Function<? super T, V> mapper) {
     if (thisIsNotValue()) {
       return (Exceptional<V>) this;
     }
-    try {
-      return Exceptional.exceptional(mapper.apply(this.value));
-    } catch (Exception e) {
-      return Exceptional.exceptional(e);
-    }
+    return getExceptional(() -> mapper.apply(this.value));
   }
 
   /**
-   * @param mapper should be not null
+   * Map the value in {@link Exceptional} to a value from another {@link Exceptional} provided by
+   * mapper. Exceptions in mapper won't be caught.
+   *
+   * @param mapper to get a new value.
+   * @param <V> type of new value.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before mapping or with exception from the {@link Exceptional} mapper returned.
    */
   @SuppressWarnings("unchecked")
   public <V> Exceptional<V> flatMap(Function<? super T, Exceptional<V>> mapper) {
@@ -92,7 +109,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @param consumer should not be null
+   * Executes some logic using not null value form the {@link Exceptional}.
+   *
+   * @param consumer consumer of the value.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before running this method or with an exception thrown by the value consumer.
    */
   public Exceptional<T> ifValue(Consumer<? super T> consumer) {
     if (thisIsNotValue()) {
@@ -102,7 +123,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @param consumer should not be null
+   * Executes some logic using not null exception form the {@link Exceptional}.
+   *
+   * @param consumer consumer of the exception.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before running this method or with an exception thrown by the exception consumer.
    */
   public Exceptional<T> ifException(Consumer<Exception> consumer) {
     if (this.isException()) {
@@ -112,7 +137,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @param runnable should not be null
+   * Executes some logic if the {@link Exceptional} is in empty state.
+   *
+   * @param runnable to execute
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before running this method or with an exception thrown by the runnable.
    */
   public Exceptional<T> ifEmpty(Runnable runnable) {
     if (!this.isValuePresent() && !this.isException()) {
@@ -122,7 +151,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @throws {@link IllegalStateException} in case if this is empty or contains an exception
+   * Get the not null value from the {@link Exceptional}.
+   *
+   * @return the value
+   * @throws IllegalStateException if the {@link Exceptional} contains exception or it's in empty
+   * state.
    */
   public T getValue() throws IllegalStateException {
     if (this.isException()) {
@@ -135,7 +168,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @throws {@link IllegalStateException} in case if this is empty or contains a value
+   * Get the not null exception from the {@link Exceptional}.
+   *
+   * @return the exception
+   * @throws IllegalStateException if the {@link Exceptional} contains value or it's in empty
+   * state.
    */
   public Exception getException() throws IllegalStateException {
     if (!this.isException()) {
@@ -145,7 +182,11 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @param defaultValue should be not null
+   * Get the value from the {@link Exceptional} or default value if the {@link Exceptional} contains
+   * exception or in empty state.
+   *
+   * @param defaultValue to return if the {@link Exceptional} contains exception or in empty state.
+   * @return the value or default value.
    */
   public T getOrDefault(T defaultValue) {
     if (this.thisIsNotValue()) {
@@ -155,29 +196,61 @@ public final class Exceptional<T> {
   }
 
   /**
-   * @param mapper should be not null
+   * Map the exception in {@link Exceptional} to some value catching all the exceptions from
+   * mapper.
+   *
+   * @param mapper to get a new value from the exception.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * occurred in process of mapping.
    */
   public Exceptional<T> resumeOnException(ExceptionalFunction<Exception, T> mapper) {
     if (!this.isException()) {
       return this;
     }
-    return Exceptional.getExceptional(() -> mapper.apply(this.exception));
+    return getExceptional(() -> mapper.apply(this.exception));
   }
 
   /**
-   * @param mapper should be not null
+   * Map the exception in the {@link Exceptional} to some other exception. Exceptions in mapper
+   * won't be caught.
+   *
+   * @param mapper to get a new exception
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * returned by the mapper.
    */
   public Exceptional<T> mapException(Function<Exception, Exception> mapper) {
     if (!this.isException()) {
       return this;
     }
-    return Exceptional.exceptional(mapper.apply(this.exception));
+    return exceptional(mapper.apply(this.exception));
   }
 
+  /**
+   * Executes some logic using not null exception form the {@link Exceptional} once per the
+   * exception.
+   *
+   * @param consumer consumer of the exception.
+   * @return an instance of {@link Exceptional} with value or in empty state or with an exception
+   * caught before running this method or with an exception thrown by the exception consumer.
+   */
+  public Exceptional<T> handleException(Consumer<Exception> consumer) {
+    if (!this.isException() || isExceptionHandled) {
+      return this;
+    }
+    this.isExceptionHandled = true;
+    return executeSafely(() -> consumer.accept(this.exception));
+  }
+
+  /**
+   * @return true if the {@link Exceptional} contains an exception.
+   */
   public boolean isException() {
     return exception != null;
   }
 
+  /**
+   * @return true if the {@link Exceptional} contains a not null value.
+   */
   public boolean isValuePresent() {
     return this.value != null;
   }
